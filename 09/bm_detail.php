27a3d396458affdd9f35ff1bd3c440bbc2d6c38a
masaakitsuchiya <zenmon.tiger.koumon.wolf@gmail.com>
2017-02-17 01:25:04 +0900

    09 kadai modified ver5

--- 09/bm_detail.php
+++ 09/bm_detail.php
@@ -34,19 +34,16 @@ if($status==false){
 <head>
   <meta charset="UTF-8">
   <title>データ編集</title>
+  <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <style>div{padding: 10px;font-size:16px;}</style>
 </head>
 <body>
 
 <!-- Head[Start] -->
-<header>
-  <nav class="navbar navbar-default">
-    <div class="container-fluid">
-    <div class="navbar-header"><a class="navbar-brand" href="bm_list_view.php">データ一覧</a></div>
-    </div>
-  </nav>
-</header>
+<?php include("./template/nav.html"); ?>
 <!-- Head[End] -->
 
 <!-- Main[Start] -->
